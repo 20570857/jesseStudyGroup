c826fe7842687991485e960b960d82e4aa6c40c5
jesse <jesse@DESKTOP-NRR7I3S>
2023-03-13 17:47:47 +0200

    Add project files.

---
+++ jesseStudyGroup/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using jesseStudyGroup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jesseStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        static IList<Student> studentList = new List<Student>
+        {
+            new Student() { StudentNumber = "u20570857", StudentName = "Jesse", StudentSurname = "Kotze", StudentEmail = "jesse.kotze@tuks.co.za" } ,
+            new Student() { StudentNumber = "u19054708", StudentName = "John", StudentSurname = "Doe", StudentEmail = "john.doe@tuks.co.za" } ,
+            new Student() { StudentNumber = "u18652589", StudentName = "Dillan", StudentSurname = "Drake", StudentEmail = "dillan.drake@tuks.co.za" } ,
+            new Student() { StudentNumber = "u20258450", StudentName = "Kate", StudentSurname = "Moss", StudentEmail = "kate.moss@tuks.co.za" } ,
+            new Student() { StudentNumber = "u21055848", StudentName = "Frank", StudentSurname = "Felix", StudentEmail = "frank.felix@tuks.co.za" }
+        };
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(studentList);
+        }
+    }
+}
---
+++ jesseStudyGroup/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace jesseStudyGroup.Models
+{
+    public class Student
+    {
+        [Display(Name = "STUDENT NUMBER")]
+        public string StudentNumber { get; set; }
+
+        [Display(Name = "NAME")]
+        public string StudentName { get; set; }
+
+        [Display(Name = "SURNAME")]
+        public string StudentSurname { get; set; }
+
+        [Display(Name = "EMAIL ADDRESS")]
+        public string StudentEmail { get; set; }
+    }
+}
